bca603620cf36d93ac4872cff64f11e61b36c12c
Anshuman Yadav <ianshumanyadav@gmail.com>
2021-10-25 22:11:23 +0530

    Added getParticipantName, getAnswers and submitScore to quizService.ts

--- src/app/shared/quiz.service.ts
+++ src/app/shared/quiz.service.ts
@@ -11,12 +11,18 @@ export class QuizService {
   seconds!: number;
   timer!:any;
   qnProgress!: number;
+  correctAnswersCount: number = 0;
 
   // Helper methods
   constructor(private http: HttpClient) { }
 
   displayTimeElasped() {
     return (Math.floor(this.seconds/3600).toLocaleString(undefined,{minimumIntegerDigits: 2})) + ':' + (Math.floor(this.seconds/60).toLocaleString(undefined,{minimumIntegerDigits: 2}))+ ':' + (Math.floor(this.seconds % 60).toLocaleString(undefined,{minimumIntegerDigits: 2}));
+  }
+
+  getParticipantName() {
+    var participant: any = JSON.stringify(localStorage.getItem('participant'));
+    return participant.Name;
   }
 
   // Http Methods
@@ -31,4 +37,17 @@ export class QuizService {
   getQuestions() {
     return this.http.get(this.rootUrl + '/api/Questions');
   }
+
+  getAnswers() {
+    var body = this.qns.map(x => x.QnId);
+    return this.http.post(this.rootUrl + '/api/answers', body);
+  }
+
+  submitScore() {
+    var body: any = JSON.stringify(localStorage.getItem('participant'));
+    body.Score = this.correctAnswersCount;
+    body.TimeSpent = this.seconds;
+    return this.http.post(this.rootUrl + "/api/UpdateOutput", body);
+  }
+
 }
